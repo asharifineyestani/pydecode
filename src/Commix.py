dc5c7c8f699972dc932c6ef3fdc0869b7694c273
Ali Sharifi Neyestani <sh4rifi@gmail.com>
2021-06-06 22:55:04 +0430

    Added replace_files

--- src/Commix.py
+++ src/Commix.py
@@ -1,3 +1,6 @@
+import os, fnmatch
+
+
 def reverse_dictionary(dictionary):
     return {v: k for k, v in dictionary.items()}
 
@@ -7,3 +10,31 @@ class Commix:
     def __init__(self, actions):
         self.actions = actions
         self.return_to_original = False
+
+    @staticmethod
+    def replace_files(action, find, replace):
+
+        if 'find_pattern' in action:
+            find = action['find_pattern'].replace('?', find)
+            replace = action['find_pattern'].replace('?', replace)
+
+        if 'file_path' in action:
+            if os.path.exists(action['file_path']):
+                with open(action['file_path']) as f:
+                    s = f.read()
+                s = s.replace(find, replace)
+                with open(action['file_path'], "w") as f:
+                    f.write(s)
+
+        elif 'directory' in action:
+            for path, dirs, files in os.walk(os.path.abspath(action['directory'])):
+                for filename in fnmatch.filter(files, action['file_pattern']):
+                    filepath = os.path.join(path, filename)
+                    with open(filepath) as f:
+                        s = f.read()
+                    s = s.replace(find, replace)
+                    with open(filepath, "w") as f:
+                        f.write(s)
+        return True
+
+
